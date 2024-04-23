a9b1c70473b66224401d191dde702cd69d1a02f1
Inumikael <159866200+Inumikael@users.noreply.github.com>
2024-04-22 19:05:34 -0600

    Add files via upload

---
+++ EVA3_1_EXCEPCIONES/src/eva3_1_excepciones/EVA3_1_EXCEPCIONES.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_excepciones;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA3_1_EXCEPCIONES {
+
+    /**
+     * @param args the command line arguments  
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num; 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Dame un numero que sea entero");
+        num = scan.nextInt();
+        System.out.println("El valor es " + num); 
+        
+    }
+    
+}
